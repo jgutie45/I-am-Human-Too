07e8ccc8363fd2411d855607956c95afeb4f19fb
Xinyu Wu <xwu82@uic.edu>
2021-04-06 19:48:21 -0500

    added 3 AI to main

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,71 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
     // Basic
-    public NavMeshAgent agent;
+    public float rotationSpeed = 60f;
+    List<Transform> visibleTargets;
+    public UnityEngine.AI.NavMeshAgent agent;
     public Transform player;
-    public LayerMask whatIsGround, whatIsPlayer;
+    public LayerMask Player;
 
     // Patrolling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
+    [HideInInspector]
+    public Vector3 patrollingSpot;
+    bool atPatrollingSpot;
 
     // Attacking
-    public float timeBetweenAttacks;
+    public float timeBetweenAttacks = 1f;
     bool alreadyAttacked;
 
     // States
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-    
+    public float attackRange = 1.25f;
+    bool playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        visibleTargets = GetComponent<FieldOfView>().visibleTargets;
+        // patrollingSpot = new Vector3(11f, transform.position.y, 2f);
+        patrollingSpot = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-        
-        if (!playerInSightRange && !playerInAttackRange) Patrolling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInSightRange && playerInAttackRange) AttackPlayer();
-    }
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, Player);
 
-    private void Awake()
-    {
-        player = GameObject.Find("Player").transform;
-        agent = GetComponent<NavMeshAgent>();
+        // player is not insight nor in attack range
+        if(visibleTargets.Count == 0 && !playerInAttackRange) Patrolling();
+        // player within sight, but not in attack range, should chase player now
+        if(visibleTargets.Count != 0 && !playerInAttackRange) ChasePlayer();
+        // player in sight and in attack range, hit him now
+        if(visibleTargets.Count != 0 && playerInAttackRange) AttackPlayer();
+
     }
 
     private void Patrolling()
     {
-        if (!walkPointSet) SearchWalkPoint();
-        if (walkPointSet) agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        // Walkpoint reached, then set false, find another walkpoint
-        if (distanceToWalkPoint.magnitude < 1f) walkPointSet = false;
-    }
-
-    private void SearchWalkPoint()
-    {
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-        // Check if random walkpoint is out side of map
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround)) walkPointSet = true;
+        Vector3 dis2patrollingSpot = transform.position - patrollingSpot;
+        if(dis2patrollingSpot.magnitude < 1f)
+        {
+            // Debug.Log("AI at the patrolling spot");
+            transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // back to the patrolling spot
+            agent.SetDestination(patrollingSpot);
+        }
     }
 
     private void ChasePlayer()
@@ -95,9 +94,7 @@ public class EnemyAI : MonoBehaviour
     // Editor visualization helper
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
